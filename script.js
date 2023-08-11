e5c186dcc4d1428ce2b6d0e7a46b90fbd9c80f16
Nathaniel Rooney <rooney20@bu.edu>
2023-08-11 14:59:42 -0400

    Updated game with new features and improvements

--- script.js
+++ script.js
@@ -1,32 +1,71 @@
-let currentWord;
-let anagrams;
+let currentWord = '';
+let anagrams = [];
 let currentAnagramIndex = 0;
+let timeLeft = 60;  // 60 seconds
+let timerInterval;
+let score = 0;
 
-function startGame() {
-    currentWord = document.getElementById('wordInput').value;
-    if (!currentWord) {
-        alert("Please enter a word to start the game!");
-        return;
+function shuffle(word) {
+    let shuffledWord = word.split('').sort(() => 0.5 - Math.random()).join('');
+    return shuffledWord;
+}
+
+function generateAnagrams(word) {
+    let anagramSet = new Set();
+    while (anagramSet.size < 3) {
+        anagramSet.add(shuffle(word));
     }
+    return [...anagramSet];
+}
 
-    // For simplicity, let's just shuffle the letters to create a single anagram.
-    // In a real game, you'd generate multiple valid anagrams.
-    anagrams = [shuffle(currentWord)];
-    currentAnagramIndex = 0;
-    displayAnagram();
+function getScoreForWord(word) {
+    switch (word.length) {
+        case 3: return 100;
+        case 4: return 400;
+        case 5: return 1200;
+        case 6: return 2000;
+        case 7: return 3000;
+        default: return 0;  // No points for words outside the specified lengths
+    }
+}
 
-    document.getElementById('anagramDisplay').classList.remove('hidden');
+function updateScore(word) {
+    score += getScoreForWord(word);
+    document.getElementById('score').innerText = "Score: " + score;
+}
+
+function startTimer() {
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        document.getElementById('timer').innerText = "Time: " + timeLeft;
+        if (timeLeft <= 0) {
+            clearInterval(timerInterval);
+            endGame();
+        }
+    }, 1000);
 }
 
 function displayAnagram() {
     document.getElementById('currentAnagram').innerText = anagrams[currentAnagramIndex];
 }
 
+function endGame() {
+    clearInterval(timerInterval);
+    document.getElementById('anagramDisplay').classList.add('hidden');
+    document.getElementById('feedback').innerText = "Game over! Your score is: " + score;
+}
+
 function checkGuess() {
     const guess = document.getElementById('guessInput').value;
+    
+    // Check for minimum word length
+    if (guess.length < 3) {
+        document.getElementById('feedback').innerText = "Enter a word with at least 3 letters!";
+        return;
+    }
+    
     if (guess === currentWord) {
-        document.getElementById('feedback').innerText = "Correct!";
-        // Move to next anagram or end game if out of anagrams.
+        updateScore(guess);
         currentAnagramIndex++;
         if (currentAnagramIndex < anagrams.length) {
             displayAnagram();
@@ -38,12 +77,21 @@ function checkGuess() {
     }
 }
 
-function endGame() {
-    document.getElementById('anagramDisplay').classList.add('hidden');
-    alert("Congratulations! You've guessed all the anagrams.");
-}
+function startGame() {
+    currentWord = document.getElementById('wordInput').value;
+    if (!currentWord || currentWord.length < 3) {
+        alert("Please enter a word with at least 3 letters to start the game!");
+        return;
+    }
 
-// Simple function to shuffle letters in a word
-function shuffle(word) {
-    return word.split('').sort(() => 0.5 - Math.random()).join('');
+    anagrams = generateAnagrams(currentWord);
+    currentAnagramIndex = 0;
+    score = 0;
+    timeLeft = 60;
+    document.getElementById('score').innerText = "Score: 0";
+    document.getElementById('timer').innerText = "Time: 60";
+    displayAnagram();
+    startTimer();
+
+    document.getElementById('anagramDisplay').classList.remove('hidden');
 }
